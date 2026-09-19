ab797b15b797e2c3df2b81dc545b8f87e5b2b867
flmngwllm <flmngwllm232@gmail.com>
2026-09-19 14:10:40 -0400

    renamed subnet_id output to aks_subnet_id for clarity

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -2,7 +2,7 @@ output "vnet_id" {
     value = azurem_virtual_network.resilienceforge_vnet.id
 }
 
-output "subnet_id" {
+output "aks_subnet_id" {
     value = azurerm_subnet.resilienceforge_subnet.id
 }
 
