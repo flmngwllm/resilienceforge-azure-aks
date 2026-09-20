c81366c0bbabe59e1905116ae7609979b440eb31
flmngwllm <flmngwllm232@gmail.com>
2026-09-19 21:45:47 -0400

    update requirements for httpx2 and add health check tests

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -7,4 +7,14 @@ def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "App front page"}
-    
+
+def test_ready():
+    response = client.get("/health/live")
+    assert response.status_code == 200
+    assert response.json() == {"status": "alive"}
+
+
+def test_live():
+    response = client.get("/health/ready")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ready"}
